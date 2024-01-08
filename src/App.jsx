b781db8eb4d15bd18b2150ad08118953aa2a2337
Smith <bsmith672@rku.ac.in>
2024-01-08 22:15:28 +0530

    in progress of ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 export default function App() {
   return (
   <>
-    
+  <div className='fullscreen'>
+  <input className = "input" type='text' placeholder='Enter location'></input>
+  <div className='weathershow'>
+  <div className='placentemp'>
+    <div className='place'>London,GB</div>
+    <div className='weathertype'>scattered cloud</div>
+    <div className='weather'>10</div>
+    </div>
+    <div className='weatherproperty'>
+    <img src='' alt="cloud"></img>
+    <div className='allabout'>
+    <div>feels like</div>
+    <div>humidity</div>
+    <div>wind</div>
+    </div>
+    </div>
+  </div>
+  </div>
   </>
   )
 }
